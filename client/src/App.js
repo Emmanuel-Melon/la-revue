9180c3711bbe4521fa809f9c53c5594e0442c267
Eman <emmanuelgatwech@gmail.com>
2019-11-22 12:05:57 +0300

    added about and restaurant pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,92 +1,39 @@
 import React, { Component } from 'react'
 import './App.css'
-import { FaFilter } from 'react-icons/fa'
 
-/**
- * components
- */
-import Filter from './Components/Filter'
-import ListRestaurants from './Components/ListRestaurants'
-import Navabar from './Components/Navbar'
-import Map from './Components/Map'
-import SearchRestarants from './Components/Search'
+// improve ux by adding instructions above the filter button
+// click on map to add restaurant
+// how to use page
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from "react-router-dom"
 
 /**
  * services
  */
 import Geolocation from './Services/Geolocation'
 
-/**
- * utils
- */
-import API from './Utils/api'
+import AboutScreen from './Screens/About'
+import HomeScreen from './Screens/Home'
+import RestaurantsScreen from './Screens/Restaurants'
+import Navabar from "./Components/Navbar"
+
 
 
 // use context to set user current location
 class App extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      hidden: true
-    }
-  }
-  componentDidMount () {
-    this.getReviews()
-  }
-
-  async getReviews () {
-    try {
-      const api = new API('/reviews')
-      const reviews = await api.fetchData()
-      console.log(reviews)
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  showControls = () => {
-    console.log(this.state)
-    this.setState(previousState  => {
-      return Object.assign({}, previousState, {
-        hidden: !previousState.hidden
-      })
-    })
-  }
-
   render () {
     return (
-      <div>
+      <Router>
         <Navabar />
-        <main className='wrapper'>
-          <section className='map'>
-            <Map
-              isMarkerShown
-              googleMapURL='https://maps.googleapis.com/maps/api/js?key=AIzaSyCj2IDnv8a9yaw4XPRSO4JgKYMuyqWhsEs'
-              loadingElement={<div style={{ height: `100%` }} />}
-              containerElement={<div style={{ height: `100%` }} />}
-              mapElement={<div style={{ height: `100%` }} />}
-            />
-          </section>
-          <section className='sidebar'>
-            <div className='controls'>
-              <h1 className='header'>Restaurants in Kampala</h1>
-              <div>
-                <button onClick={this.showControls} className='toggle'><FaFilter /> Filter Restaurants</button>
-              </div>
-            </div>
-            {
-              !this.state.hidden ? (
-                <div>
-                  <SearchRestarants />
-                  <Filter />
-                </div>
-              ) : null
-            }
-            <ListRestaurants />
-          </section>
-        </main>
-      </div>
+      <Switch>
+        <Route exact path="/" component={HomeScreen} />
+        <Route exact path="/about" component={AboutScreen} />
+        <Route exact path="/restaurants" component={RestaurantsScreen} />
+      </Switch>
+      </Router>
     )
   }
 }
--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,15 +1,16 @@
 import React from 'react'
+import { NavLink } from "react-router-dom"
 
 const Navbar = () => {
   return (
     <header className='navbar'>
       <div>
-        <h3 className='navbrand'>La Revue</h3>
+        <h3><NavLink to='/' className='navbrand'>La Revue</NavLink></h3>
       </div>
       <div>
         <ul className='nav'>
-          <li className='link'>About</li>
-          <li className='link'>Restaurants</li>
+          <li><NavLink to='/about' className='link'>About</NavLink></li>
+          <li><NavLink to='/restaurants' className='link'>Restaurants</NavLink></li>
         </ul>
       </div>
     </header>
