2be2577d39592b64213d07dd4ae1c61aeca391c1
Eman <emmanuelgatwech@gmail.com>
2019-11-22 20:14:03 +0300

    fixed about page

--- client/src/Screens/About.js
+++ client/src/Screens/About.js
@@ -3,10 +3,10 @@ import React from 'react'
 const About = () => {
   return (
     <main className='about'>
-      <section>
+      <section className='about-content'>
         <div className='about-header'>
           <h3>About La Revue</h3>
-          <p>A fictional restaurant reviewing website where users could anonymously add and review restaurants. </p>
+          <p>A fictional restaurant reviewing website that allows users to anonymously add and review restaurants. </p>
 
           <p> Desipte its fictious nature, La Revue also provides real restaurant dta but doesn't allow you to alter any of that.
           You only alter what you create.
@@ -14,7 +14,7 @@ const About = () => {
         </div>
         <div className='about-body'>
           <div className='image'>
-            <img src='https://images.unsplash.com/photo-1481833761820-0509d3217039?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80' alt='' className='dummy' />
+            <img src='https://images.unsplash.com/photo-1526234362653-3b75a0c07438?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80' alt='' className='dummy' />
           </div>
           <div className='info'>
             <h3>How to use</h3>
@@ -22,6 +22,7 @@ const About = () => {
               <li>You can add fake restaurants in any given location</li>
               <li>It uses geolocation information to detect nearby restaurants</li>
             </ul>
+            <button className='submit'>Get Started</button>
 
             <h3>Disclaimer</h3>
             <p>User added restaurants are purely fictional and will not be added to Google maps. <br/>
@@ -32,15 +33,7 @@ const About = () => {
         </div>
       </section>
       <footer>
-        <div>
-          <p>Info</p>
-        </div>
-        <div>
-          <p>stack used</p>
-        </div>
-        <div>
-          <p>contact info</p>
-        </div>
+        <p>OpenClassrooms 2019</p>
       </footer>
     </main>
   )
