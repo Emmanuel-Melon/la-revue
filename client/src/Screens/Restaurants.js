86318527ac474325695856632fe66a6534e4dbe5
Eman <emmanuelgatwech@gmail.com>
2019-11-24 07:33:59 +0300

    fixed restaurants screen layout

--- client/src/Screens/Restaurants.js
+++ client/src/Screens/Restaurants.js
@@ -1,19 +1,57 @@
 import React from 'react'
 
+/**
+ * components
+ */
 import RestaurantSummary from '../Components/RestaurantSummary'
+
 import data from '../Components/data'
+
+/**
+ * styles
+ */
+import styled from 'styled-components'
+const Horizontal = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 1.5em;
+  overflow-x: scroll;
+`
+
+const Header = styled.h3`
+  color: gold;
+  text-align: center;
+`
+
 
 const RestaurantsScreen = () => {
   return (
-    <div className='vert'>
+    <main>
+      <section>
+        <Header>Recommended</Header>
+        <Horizontal>
 
-      {
-        data.results.map(restaurant => <RestaurantSummary
-          key={restaurant.id}
-          restaurant={{ ...restaurant }}
-        />)
-      }
-    </div>
+          {
+            data.results.map(restaurant => <RestaurantSummary
+              key={restaurant.id}
+              restaurant={{ ...restaurant }}
+            />)
+          }
+        </Horizontal>
+      </section>
+      <section>
+        <Header>Near Me</Header>
+        <Horizontal>
+          {
+            data.results.map(restaurant => <RestaurantSummary
+              key={restaurant.id}
+              restaurant={{ ...restaurant }}
+            />)
+          }
+        </Horizontal>
+      </section>
+    </main>
   )
 }
 
