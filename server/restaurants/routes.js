23b83ef7092f4518ca9668e6f50af21f79b53d7d
Eman <emmanuelgatwech@gmail.com>
2019-11-29 16:36:02 +0300

    updated crud methods

--- server/restaurants/routes.js
+++ server/restaurants/routes.js
@@ -5,6 +5,7 @@ const axios = require('axios')
 /**
  * controllers
  */
+const addRestaurant = require('./controllers/addRestaurant.controller')
 const getRestaurants = require('./controllers/getRestauarants.controller')
 
 /************************************************************************
@@ -18,5 +19,13 @@ const getRestaurants = require('./controllers/getRestauarants.controller')
  * @type {Router}
  */
 restaurantsRouter.post('/', getRestaurants)
+
+/**
+ * @access public
+ * @description adds and retrieves all restaurants from MongoDB
+ * @method POST
+ * @type {Router}
+ */
+restaurantsRouter.post('/add', addRestaurant)
 
 module.exports = restaurantsRouter
